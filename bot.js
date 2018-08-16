d46fd1d94a92ca92930e8769b545e1675fa2f172
musicstore12 <42451463+musicstore12@users.noreply.github.com>
2018-08-16 22:37:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '!';
 client.on('ready', function() {
-    client.user.setGame(`اكتب اي شي هنا`,"http://twitch.tv/sytra_ayman")
+    client.user.setGame(`AG STORE.`,"http://twitch.tv/sytra_ayman")
     console.log(`i am ready ${client.user.username}`);
 });
 /*
